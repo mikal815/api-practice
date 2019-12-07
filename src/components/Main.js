4b3d9e564e2bd4c0e6afc0ec0945ea3d8aef94c5
Mike M <mikal815@users.noreply.github.com>
2019-12-07 15:25:18 -0500

    Use Sass to animate button

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Main.css'
+
+class Main extends Component {
+    render() {
+        return (
+            <button className="title1">
+                Hola!
+            </button>
+        );
+    }
+}
+
+export default Main;
